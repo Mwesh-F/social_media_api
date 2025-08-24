b17264325258cde09a72d31bf6fd10d7fbc87b71
Frida Mwende <wawirafrida20@gmail.com>
2025-08-24 10:42:26 +0300

    Add posts app with Post and Comment models

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,12 @@
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from .models import User
+from .serializers import UserSerializer
+
 class ProfileView(APIView):
 	permission_classes = [IsAuthenticated]
 
